03ee26d2c87436b638915745ba5746a538421eea
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-23 17:02:12 +0000

    Added in four regular expressions to test

--- shunt.go
+++ shunt.go
@@ -14,6 +14,18 @@ func main() {
 	// Print out Infix and Postfix
 
 	// ab.c*.
-	fmt.Println("Infix: ", "a.b.c")
+	fmt.Println("Infix: ", "a.b.c*")
+	fmt.Println("Postfix: ")
+
+	// (a.(b|d))*
+	fmt.Println("Infix: ", "(a.(b|d))*")
+	fmt.Println("Postfix: ")
+
+	// a.(b|d).c*
+	fmt.Println("Infix: ", "a.(b|d).c*")
+	fmt.Println("Postfix: ")
+
+	// a.(b.b)+.c
+	fmt.Println("Infix: ", "a.(b.b)+.c")
 	fmt.Println("Postfix: ")
 }
