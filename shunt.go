0a7f1ad6e0cfb9b1a6321e61a3f7fa8fb9da6afa
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-23 16:40:13 +0000

    Added intopost function

--- shunt.go
+++ shunt.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+func intopost(infix string) string (
+
+    pofix := ""
+
+    return pofix
+)
+
 func main() {
 
     // Print out Infix and Postfix
-    fmt.Println("Infix: ")
+
+    // ab.c*.
+    fmt.Println("Infix: ", "a.b.c")
     fmt.Println("Postfix: ")
 }
