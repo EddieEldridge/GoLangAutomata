3642d6ad8d5f51b2a3e5b5c1655966c81ea4d982
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-23 16:40:53 +0000

    Fixed braces

--- shunt.go
+++ shunt.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
-func intopost(infix string) string (
+func intopost(infix string) string {
 
-    pofix := ""
+	pofix := ""
 
-    return pofix
-)
+	return pofix
+}
 
 func main() {
 
-    // Print out Infix and Postfix
+	// Print out Infix and Postfix
 
-    // ab.c*.
-    fmt.Println("Infix: ", "a.b.c")
-    fmt.Println("Postfix: ")
+	// ab.c*.
+	fmt.Println("Infix: ", "a.b.c")
+	fmt.Println("Postfix: ")
 }
