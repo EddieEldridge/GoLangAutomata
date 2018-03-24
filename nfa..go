4bcedf757dc64c4567afad0cd548197b6b55156c
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-24 17:03:25 +0000

    Basic file structure completed

--- nfa..go
+++ nfa..go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Struct to store state
 type state struct {
 	symbol rune
@@ -13,10 +17,15 @@ type nfaFragment struct {
 	accept  *state
 }
 
+// Function to change our postfix regular expression to an NFA(Non-finite automaton)
 func postRegxpToNFA(inputString string) {
 
 }
 
 func main() {
 
+	// Set our nfa fragment to be equal to whatever regular expression we send in to our postRegxpToNFA function
+	nfaFragment := postRegxpToNFA("ab.c*|")
+
+	fmt.Println(nfaFragment)
 }
