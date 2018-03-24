ddcea753c4bda0a2e1607e914a32807dbff991f5
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-24 17:57:20 +0000

    Added some switch statemetns for our regxp characters

--- nfa..go
+++ nfa..go
@@ -23,6 +23,40 @@ func postRegxpToNFA(inputString string) *nfaFragment {
 
 	// Create nfa stack
 	nfaStack := []*nfaFragment{}
+
+	// Loop through the regular expression one rune at a time
+	for _, r := inputString{		
+		
+		// Switch statements
+		switch r {
+
+			case '.':
+				// Get the top element of the stack
+				fragment2 := nfaStack[len(nfaStack)-1]
+
+				// Remove the top element off the stack
+				nfaStack = nfaStack[:len(nfaStack)-1]
+
+				// Get the top element of the stack
+				fragment1 := nfaStack[len(nfaStack)-1]
+
+				// Remove the top element off the stack
+				nfaStack = nfaStack[:len(nfaStack)-1]
+				
+				// Join the two fragments
+				fragment1.accept.edge1 = fragment2.initial
+		
+			case '|':
+
+			case '*':
+
+			// When the character read in is not one of the above characters in our switch statement
+			default:
+		}
+	}
+
+	// Return the bottom item off the stack
+	return nfaStack[0]
 }
 
 // Main function
